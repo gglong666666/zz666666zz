f6ae861d6246264ef34c7a67caf4bc37f21e1387
gglong666666 <gglong666666@gmail.com>
2016-04-14 12:18:18 +0800

    zzz
    zzz

---
+++ C/c.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- 主機: localhost
+-- 產生時間： 2016-04-14 04:07:44
+-- 伺服器版本: 5.7.10-log
+-- PHP 版本： 5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `uploadurl`
+--
+
+CREATE TABLE `uploadurl` (
+  `longlong` varchar(50) NOT NULL,
+  `short` varchar(50) NOT NULL,
+  `pointtime` varchar(50) DEFAULT NULL,
+  `updatetime` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
